bf20ee4feab1d11c4fbab7aada7911b094cfb0d1
amertareq00 <89320218+amertareq00@users.noreply.github.com>
2023-12-11 23:00:23 +0300

    Assignment7

--- reservation/clients/views.py
+++ reservation/clients/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ProductForm
 
@@ -9,11 +9,14 @@ def add(request):
     return render(request, 'add.html')
 
 def addproduct(request):
+    if 'product_count' not in request.session:
+        request.session['product_count'] = 0
+
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            return render(request, 'product.html', {'form': form})
+            request.session['product_count'] += 1
+            return redirect('product')
     else:
         form = ProductForm()
-
-    return render(request, 'product.html', {'form': form})
+    return render(request, 'product.html', {'form': form, 'product_count': request.session['product_count']})
