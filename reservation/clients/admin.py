b9d87921a67d73b9b56de627ce088e4a5af22ce5
amertareq00 <89320218+amertareq00@users.noreply.github.com>
2023-12-26 19:18:54 +0300

    Assignment9

--- reservation/clients/admin.py
+++ reservation/clients/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Client, ClientType, Product, Order
+
+admin.site.register(ClientType)
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Order)
--- reservation/clients/models.py
+++ reservation/clients/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class ClientType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    client_type = models.ForeignKey(ClientType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, choices=[
+        ('Food', 'Food'),
+        ('Snacks', 'Snacks'),
+        ('Drinks', 'Drinks'),
+        ('Hardware', 'Hardware'),
+    ])
+    description = models.TextField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    order_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.pk} - {self.client.name}"
